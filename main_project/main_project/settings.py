c49969d8679c1fbc264568b5e066c9a2ad6c2cb3
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-12-19 02:41:29 +0600

    upd2

--- main_project/main_project/settings.py
+++ main_project/main_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--4u&zya5@+=$8!(+b@w%!93yo7!31@eq3^$_tze3p%33njq=5f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*","127.0.0.1", ".vercel.app"]
+ALLOWED_HOSTS = ["*",".vercel.app"]
 # CSRF_TRUSTED_ORIGINS = ['domainName','https://*.127.0.0.1']
 
 # Application definition
