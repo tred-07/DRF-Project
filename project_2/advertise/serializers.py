5c009aab24481481f576b4ac352d335671aa7dd3
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-12-19 02:37:17 +0600

    upd

--- project_2/advertise/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Advertise
-from django.shortcuts import render,HttpResponse
-class AdvertiseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Advertise
-        # fields=['title','description','contact']
-        fields='__all__'
