efe7475885a5ad01b819f5ee38c40156e7b7c960
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-12-19 03:02:23 +0600

    checking for ui of vercel

--- main_project/settings.py
+++ main_project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'crispy_bootstrap5',
-    # 'corsheaders'   ,
+    'corsheaders'   ,
     'advertise',
     'request',
     'review',
