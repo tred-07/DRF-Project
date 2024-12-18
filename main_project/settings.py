f23d9b96ab062f94ef940bac8d9ae1e6d60b7644
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-12-19 02:53:27 +0600

    upd requirements.txt

--- main_project/settings.py
+++ main_project/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = ["*",".vercel.app"]
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'advertise',
     'request',
     'review',
